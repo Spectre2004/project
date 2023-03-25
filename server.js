87e14ba7667dc118daa9283f4d6864c2baa20c37
Spectre <matuhinv27@gmail.com>
2023-03-25 22:54:20 +0300

    authorization

--- server.js
+++ server.js
@@ -1,17 +1,3 @@
-async function getResponse(IdUser){
-    let response = await fetch('https://api.baserow.io/api/database/rows/table/152130/?user_field_names=true', {
-        headers:{
-            Authorization: 'Token v1hcfl8ldK6GuvpNzlPXcQBEO91TeGjn'
-        }
-    })
-    let contentJson = await response.json()
-    let login = contentJson.results[IdUser].Login
-    console.log(login)
-    console.log(contentJson.results[IdUser].IdUser)
-    console.log(contentJson.results[IdUser].Login)
-    console.log(contentJson.results[IdUser].Password)
-    return login
-}
 
-let loginU = getResponse(0)
-console.log(loginU)
+
+
