8801e659db27d725837a5349961e256bec1a9969
Spectre <matuhinv27@gmail.com>
2023-03-25 18:54:42 +0300

    s

--- check.js
+++ check.js
@@ -2,6 +2,6 @@ let referrer_url = document.referrer;
 console.log(referrer_url);
 const body = document.querySelector('.body')
 
-if(referrer_url != 'http://127.0.0.1:5500/index.html'){
-     window.location.href = 'http://127.0.0.1:5500/index.html'
+if(referrer_url != 'https://spectre2004.github.io/project/'){
+     window.location.href = 'https://spectre2004.github.io/project/'
 }
