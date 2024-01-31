7575e685363410d06c0a4a3b697eef8f50825fd9
Вячеслав Матюхин <matuhinv27@gmail.com>
2024-01-31 19:58:45 +0300

    onas fix

---
+++ check.js
@@ -0,0 +1,9 @@
+//let referrer_url = document.referrer;
+//console.log(referrer_url);
+//const body = document.querySelector('.body')
+
+//if(referrer_url != 'https://spectre2004.github.io/project/'){
+//     window.location.href = 'https://spectre2004.github.io/project/'
+//}
+
+
